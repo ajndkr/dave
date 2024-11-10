029adec4fb86c108f989d8a1b5520847dec961e1
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2024-11-10 18:11:27 +0100

    💡 add comments

--- src/main.rs
+++ src/main.rs
@@ -19,16 +19,21 @@ fn gum_command() -> Result<(), Box<dyn std::error::Error>> {
     // check if gun exists, if not: panic!
     which("gum").expect("gum not found. Please install gum first.");
 
+    // spawn new process, required due to tty interaction
     let gum_proc = Command::new("gum")
         .args(["choose", "apple", "banana", "cherry"])
         .stdout(Stdio::piped())
         .spawn()?;
 
+    // wait for process to finish and get output
     let gum_output = gum_proc.wait_with_output()?;
+
+    // parse output
     let gum_choice = String::from_utf8_lossy(&gum_output.stdout)
         .trim()
         .to_string();
     println!("selected choice: {}", gum_choice);
+
     Ok(())
 }
 
