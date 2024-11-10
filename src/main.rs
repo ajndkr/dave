843985c5702db130df780b88f8ba3277c967ffb5
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2024-11-10 17:32:19 +0100

    ⚡ update main script to run first gum command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::{Command, Stdio};
+
+use which::which;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // check if gun exists, if not: panic!
+    which("gum").expect("gum not found. Please install gum first.");
+
+    let gum_proc = Command::new("gum")
+        .args(["choose", "apple", "banana", "cherry"])
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    let gum_output = gum_proc.wait_with_output()?;
+    let gum_choice = String::from_utf8_lossy(&gum_output.stdout)
+        .trim()
+        .to_string();
+    println!("selected choice: {}", gum_choice);
+
+    Ok(())
 }
