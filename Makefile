99c62f724179bac0cc2bb9f2323cbae6440bddd7
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2025-01-11 11:06:47 +0100

    ref: replace gum with inquire (#2)
    * 🔧 update makefile
    
    * ⬆️ upgrade dependencies
    
    * ➕ add inquire dependency
    
    ➖ remove which dependency
    
    * ⚡ replace gum command with inquire command
    
    * 📝 update README

--- Makefile
+++ Makefile
@@ -1,15 +1,19 @@
-.PHONY: help check fmt lint all dev clean
+.PHONY: help ci dev install clean
 
 help:		## help menu
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}'
 
-ci:			## run linters
-	cargo fmt
-	cargo check
-	cargo clippy -- -D warnings
+ci:			## run ci checks (formatting, compilation checks, linting and security checks)
+	cargo fmt --all
+	cargo check --all-targets --all-features
+	cargo clippy --all-targets --all-features -- -D warnings
+	cargo audit
 
 dev:		## build and run project
-	cargo run
+	cargo run -- $(filter-out $@,$(MAKECMDGOALS))
+
+%:
+	@:
 
 install:	## install project
 	cargo install --path .
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use clap::{CommandFactory, Parser, Subcommand};
-use std::process::{Command, Stdio};
-use which::which;
+use inquire::Select;
 
 #[derive(Parser)]
 #[command(author, version, about = "cli for automating dev workflows")]
@@ -11,29 +10,15 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    #[command(about = "choose a gum flavor")]
-    Gum {},
+    #[command(about = "choose a flavor")]
+    Choose {},
 }
 
-fn gum_command() -> Result<(), Box<dyn std::error::Error>> {
-    // check if gum exists, if not: panic!
-    which("gum").expect("gum not found. Please install gum first.");
+fn choose_command() -> Result<(), Box<dyn std::error::Error>> {
+    let choices = vec!["apple", "banana", "cherry"];
+    let choice = Select::new("pick a flavor:", choices).prompt()?;
 
-    // spawn new process, required due to tty interaction
-    let gum_proc = Command::new("gum")
-        .args(["choose", "apple", "banana", "cherry"])
-        .stdout(Stdio::piped())
-        .spawn()?;
-
-    // wait for process to finish and get output
-    let gum_output = gum_proc.wait_with_output()?;
-
-    // parse output
-    let gum_choice = String::from_utf8_lossy(&gum_output.stdout)
-        .trim()
-        .to_string();
-    println!("selected choice: {}", gum_choice);
-
+    println!("selected choice: {}", choice);
     Ok(())
 }
 
@@ -41,7 +26,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
     match cli.command {
-        Some(Commands::Gum {}) => gum_command()?,
+        Some(Commands::Choose {}) => choose_command()?,
         None => {
             let _ = Cli::command().print_help();
             std::process::exit(0);
