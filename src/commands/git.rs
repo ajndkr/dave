b911ba6166c513488e6f3ec081fc65e04cfa7784
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2025-01-12 17:08:28 +0100

    ⚡ update sync function

--- src/commands/git.rs
+++ src/commands/git.rs
@@ -1,6 +1,7 @@
-use clap::Subcommand;
-
 use crate::{CliResult, Command};
+use clap::Subcommand;
+use std;
+use which::which;
 
 // define subcommands for 'git' command
 #[derive(Subcommand)]
@@ -29,6 +30,62 @@ impl Command for GitCommands {
 pub fn sync() -> CliResult<()> {
     println!("running git sync workflow...");
 
-    // add code here
+    // check if git is installed
+    which("git").expect("git not found. install git first and try again.");
+
+    println!("staging local changes...");
+    std::process::Command::new("git")
+        .arg("add")
+        .arg(".")
+        .output()
+        .expect("failed to stage local changes");
+
+    println!("fetching remote changes...");
+    std::process::Command::new("git")
+        .arg("fetch")
+        .arg("-p")
+        .output()
+        .expect("failed to fetch remote changes");
+
+    println!("stashing local changes...");
+    std::process::Command::new("git")
+        .arg("stash")
+        .output()
+        .expect("failed to stash local changes");
+
+    println!("pulling remote changes...");
+    std::process::Command::new("git")
+        .arg("pull")
+        .arg("--rebase")
+        .output()
+        .expect("failed to pull remote changes");
+
+    println!("restoring local changes...");
+    std::process::Command::new("git")
+        .arg("stash")
+        .arg("pop")
+        .output()
+        .expect("failed to restore local changes");
+
+    std::process::Command::new("git")
+        .arg("stash")
+        .arg("clear")
+        .output()
+        .expect("failed to clear stash");
+
+    println!("git sync complete! latest commit:");
+
+    let git_log_output = std::process::Command::new("git")
+        .arg("log")
+        .arg("-1")
+        .output()
+        .expect("failed to get latest commit");
+
+    let latest_commit = String::from_utf8_lossy(&git_log_output.stdout)
+        .trim()
+        .to_string();
+
+    println!("{}", latest_commit);
+
     Ok(())
 }
