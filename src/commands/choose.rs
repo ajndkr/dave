78d5cc46bd32f450bebeb4485401c225af91e95e
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2025-01-12 12:52:15 +0100

    💡 add docstring to choose command

--- src/commands/choose.rs
+++ src/commands/choose.rs
@@ -2,6 +2,8 @@ use crate::error::CliResult;
 
 use inquire::Select;
 
+// choose a flavor using 'inquire.Select'
+// 'inquire.Select' creates an interactive prompt for selecting a choice from a list
 pub fn choose_command() -> CliResult<()> {
     let choices = vec!["apple", "banana", "cherry"];
     let choice = Select::new("pick a flavor:", choices).prompt()?;
