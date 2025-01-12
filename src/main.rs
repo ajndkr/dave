0f48cf2df2feed2466895fe750abee88d5ef6e9a
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2025-01-12 16:56:28 +0100

    ⚡ add git commands and shell completions generator

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use clap::{CommandFactory, Parser, Subcommand};
-use devx::{choose, CliResult, Command, ManageCommands};
+use clap::{value_parser, ArgAction, CommandFactory, Parser, Subcommand};
+use clap_complete::{generate, Shell};
+use devx::{choose, CliResult, Command, GitCommands, ManageCommands};
+use std::io;
 
 #[derive(Parser)]
 #[command(author, version, about = "cli for automating dev workflows")]
 struct Cli {
     #[clap(subcommand)]
     command: Option<Commands>,
+
+    #[arg(long = "generate", action = ArgAction::Set, value_parser = value_parser!(Shell))]
+    generator: Option<Shell>,
 }
 
 #[derive(Subcommand)]
@@ -17,8 +22,14 @@ enum Commands {
         #[clap(subcommand)]
         command: Option<ManageCommands>,
     },
+    #[command(about = "run git workflows")]
+    Git {
+        #[clap(subcommand)]
+        command: Option<GitCommands>,
+    },
 }
 
+// prints help page for invalid subcommands
 fn handle_invalid_subcommand(subcommand: &str) -> ! {
     let mut cmd = Cli::command();
 
@@ -30,13 +41,34 @@ fn handle_invalid_subcommand(subcommand: &str) -> ! {
     std::process::exit(0);
 }
 
+// generates completions script for input shell
+fn print_completions(generator: Shell, cmd: &mut clap::Command) {
+    generate(
+        generator,
+        cmd,
+        cmd.get_name().to_string(),
+        &mut io::stdout(),
+    );
+}
+
 fn main() -> CliResult<()> {
     let cli = Cli::parse();
 
+    // define `--generate` flag
+    if let Some(generator) = cli.generator {
+        let mut cmd = Cli::command();
+        print_completions(generator, &mut cmd);
+        return Ok(());
+    }
+
+    // match commands and subcommands
     match cli.command {
         Some(Commands::Choose {}) => choose()?,
         Some(Commands::Manage { command }) => {
             command.map_or_else(|| handle_invalid_subcommand("manage"), |cmd| cmd.execute())?
+        }
+        Some(Commands::Git { command }) => {
+            command.map_or_else(|| handle_invalid_subcommand("git"), |cmd| cmd.execute())?
         }
         None => {
             let _ = Cli::command().print_help();
