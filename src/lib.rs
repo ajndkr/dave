c790048f69e80b692ff655f5b85d3a2b92730ce7
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2025-01-12 12:52:03 +0100

    ⚡ add constants and uninstall modules to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod commands;
+pub mod constants;
 pub mod error;
 
 pub use error::{CliError, CliResult};
 
 // re-export for external use
 pub use commands::choose::choose_command;
+pub use commands::uninstall::uninstall_command;
